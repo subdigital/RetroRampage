8b459a1fa9e5c21e86e2f52df9ba1169f83ab7bc
Ben Scheirman <ben@scheirman.com>
2019-12-03 08:10:14 -0600

    Initial Commit

---
+++ RetroRampage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RetroRampage
+//
+//  Created by Ben Scheirman on 12/3/19.
+//  Copyright © 2019 Fickle Bits. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
